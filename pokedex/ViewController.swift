8e65bf42c6c3578f2cac02c02ea091638ef96920
Rossana Rocha da Silva <rossana.silva@premiersoft.net>
2022-07-13 17:15:30 -0300

    first commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Rossana Rocha on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
